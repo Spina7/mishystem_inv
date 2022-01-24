25aea01220ada19341309c33ca29a8dd7c13fa82
Sergio Piña <alonzopina7@gmail.com>
2022-01-24 12:57:36 -0600

    version inicial

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Mishystem-INV V1
+|--------------------------------------------------------------------------
+| Project Name: Mishystem
+| Version: v1
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Establecer host de base de datos
+  define( 'DB_USER', 'root' );             // Establecer usuario de base de datos
+  define( 'DB_PASS', '' );             // Establecer contraseña de base de datos
+  define( 'DB_NAME', 'mishystem_inv' );        // Establecer el nombre de la base de datos
+
+?>
