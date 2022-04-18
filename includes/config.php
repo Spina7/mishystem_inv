8a76318fc78b6032a72f20ab173c49921e267924
Sergio Piña <alonzopina7@gmail.com>
2022-04-18 17:29:49 -0500

    asd

--- includes/config.php
+++ includes/config.php
@@ -11,7 +11,5 @@
 */
   define( 'DB_HOST', 'localhost' );          // Establecer host de base de datos
   define( 'DB_USER', 'root' );             // Establecer usuario de base de datos
-  define( 'DB_PASS', '' );             // Establecer contraseña de base de datos
+  define( 'DB_PASS', 'phoenixKz7n00.' );             // Establecer contraseña de base de datos
   define( 'DB_NAME', 'mishystem_inv' );        // Establecer el nombre de la base de datos
-
-?>
