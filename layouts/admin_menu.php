f8d6a0ff6d866969895f1746990849e83189af59
Sergio Piña <alonzopina7@gmail.com>
2022-04-21 13:33:52 -0500

    lineabase_menufix

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -11,7 +11,6 @@
       <span>Accesos</span>
     </a>
     <ul class="nav submenu">
-      <li><a href="group.php">Administrar grupos</a> </li>
       <li><a href="users.php">Administrar usuarios</a> </li>
     </ul>
   </li>
@@ -31,12 +30,7 @@
       <li><a href="add_product.php">Agregar productos</a> </li>
     </ul>
   </li>
-  <li>
-    <a href="media.php">
-      <i class="glyphicon glyphicon-picture"></i>
-      <span>Media</span>
-    </a>
-  </li>
+
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-th-list"></i>
@@ -45,17 +39,6 @@
     <ul class="nav submenu">
       <li><a href="sales.php">Administrar ventas</a> </li>
       <li><a href="add_sale.php">Agregar venta</a> </li>
-    </ul>
-  </li>
-  <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-signal"></i>
-      <span>Reporte de ventas</span>
-    </a>
-    <ul class="nav submenu">
-      <li><a href="sales_report.php">Ventas por fecha </a></li>
-      <li><a href="monthly_sales.php">Ventas mensuales</a></li>
-      <li><a href="daily_sales.php">Ventas diarias</a> </li>
     </ul>
   </li>
 </ul>
